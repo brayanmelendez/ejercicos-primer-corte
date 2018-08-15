adf946dca82b7aa14736b229ada93967abee0319
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-08-15 13:49:54 -0500

    seleccion de un numero a romano

---
+++ src/Ejercicios/Ejercicio14.java
@@ -0,0 +1,59 @@
+package Ejercicios;
+
+import javax.swing.JOptionPane;
+
+public class Ejercicio14 {
+
+    public static void main(String[] args) {
+
+        int n;
+        String NR;
+        n = Integer.parseInt(JOptionPane.showInputDialog(null, "INGRESE UN NUMERO QUE VAYA DEL 1 A 10"));
+
+        switch (n) {
+
+            case (1):
+                NR = "I";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (2):
+                NR = "II";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (3):
+                NR = "III";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (4):
+                NR = "IV";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (5):
+                NR = "V";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (6):
+                NR = "VI";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (7):
+                NR = "VII";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (8):
+                NR = "VIII";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (9):
+                NR = "IX";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+                break;
+            case (10):
+                NR = "X";
+                JOptionPane.showMessageDialog(null, "EL NUMERO ROMANO ES: " + NR);
+
+        }
+
+    }
+
+}
