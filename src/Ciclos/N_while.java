1dcefae5e6836d4d0da83ea17fb020494b24aa74
brayanmelendez <brayanmelendezarnedo@gmail.com>
2018-08-20 23:48:37 -0500

    de 1 al cien con los ciclos

---
+++ src/Ciclos/N_while.java
@@ -0,0 +1,24 @@
+package Ciclos;
+
+public class N_while {
+
+    public static void main(String[] args) {
+
+        /*while (i <= 100) {
+
+            System.out.println("numero " + i);
+            i++;*/
+        //}
+        /*do {
+            System.out.println("numero " + i);
+            i++;
+        } while (i <= 100);*/
+        for (int i = 1; i <= 100; i = i+1) {
+            System.out.println("numero " + i);
+            
+
+        }
+
+    }
+
+}
